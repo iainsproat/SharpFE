a11b26f3d8784b9b58dfbd264265532d281690ed
Andreas Bak <Andreas.Bak@ramboll.co.uk>
2013-01-08 23:04:38 +0000

    Ammended test for PitchedRoofFrame
    Test fails because of unwanted displacement in X-direction

--- SharpFE.Core.Tests/Examples/Beam/BeamIn3DModel.cs
+++ SharpFE.Core.Tests/Examples/Beam/BeamIn3DModel.cs
@@ -8,49 +8,106 @@ using NUnit.Framework;
 
 namespace SharpFE.Examples.Beam
 {
-	[TestFixture]
-	public class BeamInFull3DModel
-	{
+    [TestFixture]
+    public class BeamInFull3DModel
+    {
 
-		[Test]
-		public void Frame()
-		{
-			FiniteElementModel model = new FiniteElementModel(ModelType.Full3D); // we will create and analyze a 3D frame system
-			FiniteElementNode node1 = model.NodeFactory.Create(0,0,0); // create a node at the origin
-			model.ConstrainNode(node1, DegreeOfFreedom.X);
-			model.ConstrainNode(node1, DegreeOfFreedom.Y);
-			model.ConstrainNode(node1, DegreeOfFreedom.Z);
-			model.ConstrainNode(node1, DegreeOfFreedom.XX);
-			model.ConstrainNode(node1, DegreeOfFreedom.ZZ);
-			
-			FiniteElementNode node2 = model.NodeFactory.Create(0,0,1);
-			
-			FiniteElementNode node3 = model.NodeFactory.Create(1,0,1);
-			FiniteElementNode node4 = model.NodeFactory.Create(1,0,0);
-			model.ConstrainNode(node4, DegreeOfFreedom.X);
-			model.ConstrainNode(node4, DegreeOfFreedom.Y);
-			model.ConstrainNode(node4, DegreeOfFreedom.Z);
-			model.ConstrainNode(node4, DegreeOfFreedom.XX);
-			model.ConstrainNode(node4, DegreeOfFreedom.ZZ);
+        [Test]
+        public void Frame()
+        {
+            FiniteElementModel model = new FiniteElementModel(ModelType.Full3D); // we will create and analyze a 3D frame system
+            FiniteElementNode node1 = model.NodeFactory.Create(0,0,0); // create a node at the origin
+            model.ConstrainNode(node1, DegreeOfFreedom.X);
+            model.ConstrainNode(node1, DegreeOfFreedom.Y);
+            model.ConstrainNode(node1, DegreeOfFreedom.Z);
+            model.ConstrainNode(node1, DegreeOfFreedom.XX);
+            model.ConstrainNode(node1, DegreeOfFreedom.ZZ);
+            
+            FiniteElementNode node2 = model.NodeFactory.Create(0,0,1);
+            
+            FiniteElementNode node3 = model.NodeFactory.Create(1,0,1);
+            FiniteElementNode node4 = model.NodeFactory.Create(1,0,0);
+            model.ConstrainNode(node4, DegreeOfFreedom.X);
+            model.ConstrainNode(node4, DegreeOfFreedom.Y);
+            model.ConstrainNode(node4, DegreeOfFreedom.Z);
+            model.ConstrainNode(node4, DegreeOfFreedom.XX);
+            model.ConstrainNode(node4, DegreeOfFreedom.ZZ);
 
-			IMaterial material = new GenericElasticMaterial(0, 2000, 0.2, 1000);
-			ICrossSection section = new SolidRectangle(0.5, 0.1);
-			
-			model.ElementFactory.CreateLinear3DBeam(node1, node2, material, section);
-			model.ElementFactory.CreateLinear3DBeam(node2,node3,material,section);
-			model.ElementFactory.CreateLinear3DBeam(node3,node4,material,section);
-			
-			ForceVector force = model.ForceFactory.Create(10, 0, 0,0,0,0);
-			model.ApplyForceToNode(force, node2);
-			
-			IFiniteElementSolver solver = new LinearSolver(model);
-			FiniteElementResults results = solver.Solve();
-			
-			DisplacementVector displacement = results.GetDisplacement(node2);
-			Assert.AreNotEqual(0.0, displacement.X);
-			Assert.AreEqual(0.0, displacement.Y, 0.001);
-		}
-	}
-	
+            IMaterial material = new GenericElasticMaterial(0, 1, 0, 1);
+            ICrossSection section = new SolidRectangle(1, 1);
+            
+            model.ElementFactory.CreateLinear3DBeam(node1, node2, material, section);
+            model.ElementFactory.CreateLinear3DBeam(node2,node3,material,section);
+            model.ElementFactory.CreateLinear3DBeam(node3,node4,material,section);
+            
+            ForceVector force = model.ForceFactory.Create(10, 0, 0,0,0,0);
+            model.ApplyForceToNode(force, node2);
+            
+            IFiniteElementSolver solver = new LinearSolver(model);
+            FiniteElementResults results = solver.Solve();
+            
+            DisplacementVector displacement = results.GetDisplacement(node2);
+            Assert.AreNotEqual(0.0, displacement.X);
+            Assert.AreEqual(0.0, displacement.Y, 0.001);
+        }
+        
+        /// <summary>
+        /// 10---->    (3)
+        ///            / \
+        ///           /   \
+        ///          /     \
+        ///         /       \
+        ///       (2)       (4)
+        ///        |         |
+        ///        |         |
+        ///        |         |
+        ///        |         |
+        ///       (1)       (5)
+        /// </summary>
+        [Test]
+        public void PitchedRoofPortalFrame()
+        {
+            FiniteElementModel model = new FiniteElementModel(ModelType.Full3D); // we will create and analyze a 3D frame system
+            FiniteElementNode node1 = model.NodeFactory.Create(0,0,0); 
+            model.ConstrainNode(node1, DegreeOfFreedom.X);
+            model.ConstrainNode(node1, DegreeOfFreedom.Y);
+            model.ConstrainNode(node1, DegreeOfFreedom.Z);
+            model.ConstrainNode(node1, DegreeOfFreedom.XX);
+            model.ConstrainNode(node1, DegreeOfFreedom.YY);
+            model.ConstrainNode(node1, DegreeOfFreedom.ZZ);
+    
+            FiniteElementNode node2 = model.NodeFactory.Create(0,0,10);
+            FiniteElementNode node3 = model.NodeFactory.Create(10,0,14);
+            FiniteElementNode node4 = model.NodeFactory.Create(20,0,10);
+            
+            FiniteElementNode node5 = model.NodeFactory.Create(20,0,0);
+            model.ConstrainNode(node5, DegreeOfFreedom.X);
+            model.ConstrainNode(node5, DegreeOfFreedom.Y);
+            model.ConstrainNode(node5, DegreeOfFreedom.Z);
+            model.ConstrainNode(node5, DegreeOfFreedom.XX);
+            model.ConstrainNode(node5, DegreeOfFreedom.YY);
+            model.ConstrainNode(node5, DegreeOfFreedom.ZZ);
+    
+            IMaterial material = new GenericElasticMaterial(0, 1, 0, 1);
+            ICrossSection section = new SolidRectangle(1, 1);
+            
+            model.ElementFactory.CreateLinear3DBeam(node1, node2, material, section);
+            model.ElementFactory.CreateLinear3DBeam(node2, node3, material, section);
+            model.ElementFactory.CreateLinear3DBeam(node3, node4, material, section);
+            model.ElementFactory.CreateLinear3DBeam(node4, node5, material, section);
+            
+            ForceVector force = model.ForceFactory.Create(0, 0, 10,0,0,0); //Add load of 10 Newtons in Z-direction
+            model.ApplyForceToNode(force, node3);
+            
+            IFiniteElementSolver solver = new LinearSolver(model);
+            FiniteElementResults results = solver.Solve();
+            
+            DisplacementVector displacement = results.GetDisplacement(node3);
+            
+            Assert.AreEqual(0.0, displacement.Y, 0.001);
+            Assert.AreNotEqual(0.0, displacement.Z);
+            Assert.AreEqual(0.0, displacement.X, 0.001); //Displacment of node in X-direction should be 0
+        }
+    }
 }
 
