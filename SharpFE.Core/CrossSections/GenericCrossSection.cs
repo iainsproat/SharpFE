64194712a172d4bfaad5a5ade77b1fe4144e402b
Iain Sproat <iainsproat@gmail.com>
2013-07-01 14:01:30 +0100

    Added GenericCrossSection
    Cross section to which the raw Area, Iyy, Izz etc. values can be set
    directly rather than being calculated from other properties.

--- SharpFE.Core/CrossSections/GenericCrossSection.cs
+++ SharpFE.Core/CrossSections/GenericCrossSection.cs
@@ -6,15 +6,122 @@
  */
 using System;
 
-namespace SharpFE.CrossSections
+namespace SharpFE
 {
     /// <summary>
     /// Description of GenericCrossSection.
     /// </summary>
-    public class GenericCrossSection
+    public class GenericCrossSection : ICrossSection
     {
-        public GenericCrossSection()
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="height"></param>
+        /// <param name="width"></param>
+        public GenericCrossSection(double sectionArea)
         {
+            Guard.AgainstBadArgument(() => {return sectionArea <= 0;},
+                                     "sectionArea has to be a positive, non-zero number", 
+                                     "sectionArea");
+            
+            this.Area = sectionArea;
+        }
+        
+        public GenericCrossSection(double sectionArea, double sectionSecondMomentOfAreaAroundYY)
+        {
+            Guard.AgainstBadArgument(() => {return sectionArea <= 0;},
+                                     "sectionArea has to be a positive, non-zero number", 
+                                     "sectionArea");
+            Guard.AgainstBadArgument(() => {return sectionSecondMomentOfAreaAroundYY <= 0;},
+                                     "sectionSecondMomentOfAreaAroundYY has to be a positive, non-zero number", 
+                                     "sectionSecondMomentOfAreaAroundYY");
+            
+            this.Area = sectionArea;
+            this.SecondMomentOfAreaAroundYY = sectionSecondMomentOfAreaAroundYY;
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public double MaximumDepth
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public double MaximumWidth
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public double Area
+        {
+            get;
+            private set;
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public double SecondMomentOfAreaAroundYY
+        {
+            get;
+            private set;
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public double SecondMomentOfAreaAroundZZ
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public double ExternalPerimeterLength
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public double MomentOfInertiaInTorsion
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public Coordinate2D GeometricCentroid
+        {
+            get
+            {
+                return new Coordinate2D(0, 0);
+            }
         }
     }
 }
